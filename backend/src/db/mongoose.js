8574ed5de90083b37eee34b0906e63ce7a52c681
ElizabethAdhiambo <lizeuniteradhiambo@gmail.com>
2022-10-18 22:02:28 +0300

    first init

---
+++ backend/src/db/mongoose.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv:mongodb+srv://ElizabethAdhiambo:euniter2@cluster0.ghfsrmq.mongodb.net/palour2-api")
---
+++ backend/src/models/BookAppointment.js
@@ -0,0 +1,10 @@
+const mongoose = require ("mongoose");
+const validator = require ("validator");
+
+// const  BookAppointment = mongose.model("BookAppointment"{
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//     }
+// }),
